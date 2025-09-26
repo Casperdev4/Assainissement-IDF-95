4a7161858dd777e89a9164a5e6396e18828c5ea6
Casperdev4 <allam.bilal91@gmail.com>
2025-09-26 07:26:12 +0200

    ddddddddd

--- traitement.php
+++ traitement.php
@@ -69,8 +69,8 @@ try {
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
-    $mail->setFrom('contact@webprime.fr', 'IDF 95');
-    $mail->addAddress('info.assainissement@gmail.com');
+    $mail->setFrom('contact@webprime.fr', 'Aquaserv 95');
+    $mail->addAddress('contact.aquaserv@gmail.com');
     $mail->addAddress('webprime91@hotmail.com');
     $mail->CharSet = 'UTF-8';
     $mail->isHTML(true);
